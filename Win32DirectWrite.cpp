ca6226288b236847f2268ae86f518056048b7c92
Adam Strzelecki <ono@java.pl>
2018-06-26 15:27:39 +0200

    Respect DPI switching

--- Win32DirectWrite.cpp
+++ Win32DirectWrite.cpp
@@ -1,11 +1,16 @@
 // just build with: cl Win32DirectWrite.cpp
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
+
 #include <windows.h>
 #include <windowsx.h>
 #include <commctrl.h>
 #include <d2d1.h>
 #include <d2d1helper.h>
 #include <dwrite.h>
+#include <ShellScalingApi.h>
 
 #pragma comment(lib, "user32.lib")
 #pragma comment(lib, "gdi32.lib")
@@ -28,10 +33,33 @@ DECLARE_HANDLE(DPI_AWARENESS_CONTEXT);
 #define DPI_AWARENESS_CONTEXT_SYSTEM_AWARE         ((DPI_AWARENESS_CONTEXT)-2)
 #define DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE    ((DPI_AWARENESS_CONTEXT)-3)
 #define DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2 ((DPI_AWARENESS_CONTEXT)-3)
-typedef DPI_AWARENESS_CONTEXT (WINAPI * SetThreadDpiAwarenessContextProc)(DPI_AWARENESS_CONTEXT);
+typedef DPI_AWARENESS_CONTEXT (WINAPI* SetThreadDpiAwarenessContextProc)(DPI_AWARENESS_CONTEXT);
+typedef LRESULT (WINAPI* GetDpiForMonitorProc)(HMONITOR, MONITOR_DPI_TYPE, UINT*, UINT*);
+
+static wchar_t* wszText = _wcsdup(L"1. m_pRenderTarget->DrawText(\"Hello World\");");
+UINT32 cTextLength = (UINT32)wcslen(wszText);
+
+const UINT baseDpi = 96;
+UINT dpiX = baseDpi;
+UINT dpiY = baseDpi;
+FLOAT dpiScaleX = 1.0f;
+FLOAT dpiScaleY = 1.0f;
 
 HWND CreateMainWindow(HINSTANCE hInstance);
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CreateDirectWrite();
+BOOL GetDesktopDpi();
+
+BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct);
+void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify);
+void OnSize(HWND hwnd, UINT state, int cx, int cy);
+void OnDpiChanged(HWND hwnd, UINT newXDpi, UINT newYDpi, RECT const &newScaledWindowRect);
+void OnPaint(HWND hwnd);
+void OnDestroy(HWND hwnd);
+
+BOOL RecreateRenderTarget(const HWND hwnd, const D2D1_SIZE_U& size);
+
+void Debug(LPCTSTR lpszFormat, ...);
 
 INT WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ INT nCmdShow) {
     // Initialize the common controls
@@ -46,13 +74,39 @@ INT WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     if (hUser32) {
         // EnableNonClientDpiScaling = (EnableNonClientDpiScalingProc)GetProcAddress(hUser32, "EnableNonClientDpiScaling");
         SetThreadDpiAwarenessContext = (SetThreadDpiAwarenessContextProc)GetProcAddress(hUser32, "SetThreadDpiAwarenessContext");
+        if (SetThreadDpiAwarenessContext) {
+            DPI_AWARENESS_CONTEXT previousDpiContext = SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+        }
+    } else {
+        MessageBox(NULL, "user32.dll not found.", "GetModuleHandleW Error", MB_ICONERROR);
+        return 1;
     }
-    if (SetThreadDpiAwarenessContext) {
-        DPI_AWARENESS_CONTEXT previousDpiContext = SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+
+    if (!CreateDirectWrite()) {
+        MessageBox(NULL,
+            "DirectWrite failed.",
+            "DirectWrite Error", MB_ICONERROR);
+        return 1;
     }
 
     HWND hwnd = CreateMainWindow(hInstance);
     if (hwnd == 0) {
+        return 1;
+    }
+
+    GetDpiForMonitorProc GetDpiForMonitor;
+    HMODULE hShcore = LoadLibraryW(L"shcore.dll");
+    if (hShcore) {
+        GetDpiForMonitor = (GetDpiForMonitorProc)GetProcAddress(hShcore, "GetDpiForMonitor");
+        if (GetDpiForMonitor) {
+            HMONITOR monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+            GetDpiForMonitor(monitor, MDT_EFFECTIVE_DPI, &dpiX, &dpiY);
+            dpiScaleX = (FLOAT)dpiX / baseDpi;
+            dpiScaleY = (FLOAT)dpiY / baseDpi;
+            Debug("Win32DirectWrite: GetDpiForMonitor dpiX=%d dpiY=%d dpiScaleX=%f dpiScaleY=%f", dpiX, dpiY, dpiScaleX, dpiScaleY);
+        }
+    } else {
+        MessageBox(NULL, "shcore.dll not found.", "LoadLibraryW Error", MB_ICONERROR);
         return 1;
     }
 
@@ -84,24 +138,21 @@ HWND CreateMainWindow(HINSTANCE hInstance) {
     return CreateWindowEx(0, CLASS_NAME, "Application",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT,
-        400, 400,
+        320 * dpiScaleX, 300 * dpiScaleY,
         //CW_USEDEFAULT, CW_USEDEFAULT,
         NULL, NULL, hInstance, NULL);
 };
 
-BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct);
-void OnPaint(HWND hwnd);
-void OnSize(HWND hwnd, UINT state, int cx, int cy);
-void OnDestroy(HWND hwnd);
-void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify);
+#define HANDLE_WM_DPICHANGED(hwnd, wParam, lParam, fn) ((fn)((hwnd), LOWORD(wParam), HIWORD(wParam), *((RECT*)(lParam))), 0L)
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
-        HANDLE_MSG(hwnd, WM_CREATE,  OnCreate);
-        HANDLE_MSG(hwnd, WM_PAINT,   OnPaint);
-        HANDLE_MSG(hwnd, WM_SIZE,    OnSize);
-        HANDLE_MSG(hwnd, WM_DESTROY, OnDestroy);
-        HANDLE_MSG(hwnd, WM_COMMAND, OnCommand);
+        HANDLE_MSG(hwnd, WM_CREATE,     OnCreate);
+        HANDLE_MSG(hwnd, WM_COMMAND,    OnCommand);
+        HANDLE_MSG(hwnd, WM_SIZE,       OnSize);
+        HANDLE_MSG(hwnd, WM_DPICHANGED, OnDpiChanged);
+        HANDLE_MSG(hwnd, WM_PAINT,      OnPaint);
+        HANDLE_MSG(hwnd, WM_DESTROY,    OnDestroy);
         case WM_ERASEBKGND:
             return 1;
     }
@@ -130,10 +181,6 @@ enum RenderingParams {
 
 static IDWriteRenderingParams* pRenderingParams[RenderingParamsCount] = {};
 
-static wchar_t* wszText = _wcsdup(L"1. m_pRenderTarget->DrawText(\"Hello World\");");
-//static const wchar_t* wszText = L"SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2)";
-UINT32 cTextLength = (UINT32)wcslen(wszText);
-
 template <class T> void SafeRelease(T **ppT) {
     if (*ppT) {
         (*ppT)->Release();
@@ -141,33 +188,18 @@ template <class T> void SafeRelease(T **ppT) {
     }
 }
 
-BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct) {
-    RECT rc;
-    GetClientRect(hwnd, &rc);
-    D2D1_SIZE_U size = D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top);
+FLOAT ConvertPointSizeToDIP(FLOAT points) {
+    return points * 96.0f / 72.0f;
+}
 
-    if (!SUCCEEDED(D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED,
-            &pD2DFactory)) ||
-        !SUCCEEDED(DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED,
-            __uuidof(IDWriteFactory), reinterpret_cast<IUnknown**>(&pDWriteFactory))) ||
-        !SUCCEEDED(pDWriteFactory->CreateTextFormat(
-            L"Consolas", // Font family name.
-            NULL,        // Font collection (NULL sets it to use the system font collection).
-            DWRITE_FONT_WEIGHT_REGULAR,
-            DWRITE_FONT_STYLE_NORMAL,
-            DWRITE_FONT_STRETCH_NORMAL,
-            11.0f, // 9.0f * 96.0f / 72.0f,
-            L"en-us",
-            &pTextFormat)) ||
-        //!SUCCEEDED(pTextFormat->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_CENTER)) ||
-        //!SUCCEEDED(pTextFormat->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_CENTER)) ||
-        !SUCCEEDED(pD2DFactory->CreateHwndRenderTarget(
-            D2D1::RenderTargetProperties(),
-            D2D1::HwndRenderTargetProperties(
-                hwnd,
-                size),
-            &pRT)) ||
-        !SUCCEEDED(pDWriteFactory->CreateRenderingParams(&pRenderingParams[Default])) ||
+BOOL CreateDirectWrite() {
+    return
+        SUCCEEDED(D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED,
+            &pD2DFactory)) &&
+        GetDesktopDpi() &&
+        SUCCEEDED(DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED,
+            __uuidof(IDWriteFactory), reinterpret_cast<IUnknown**>(&pDWriteFactory))) &&
+        SUCCEEDED(pDWriteFactory->CreateRenderingParams(&pRenderingParams[Default])) &&
 #define CUSTOM_RENDERING_PARAMS(params, gf, ef, rm) \
         SUCCEEDED(pDWriteFactory->CreateCustomRenderingParams( \
             pRenderingParams[Default]->GetGamma() * gf, \
@@ -175,24 +207,79 @@ BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct) {
             pRenderingParams[Default]->GetClearTypeLevel(), \
             pRenderingParams[Default]->GetPixelGeometry(), \
             rm, &pRenderingParams[params]))
-        !CUSTOM_RENDERING_PARAMS(GDIClassic,   1.f, 1.f, DWRITE_RENDERING_MODE_GDI_CLASSIC) ||
-        !CUSTOM_RENDERING_PARAMS(GDINatural,   1.f, 1.f, DWRITE_RENDERING_MODE_GDI_NATURAL) ||
-        !CUSTOM_RENDERING_PARAMS(Natural,      1.f, 1.f, DWRITE_RENDERING_MODE_NATURAL    ) ||
-        !CUSTOM_RENDERING_PARAMS(Contrast2x,   1.f, 2.f, DWRITE_RENDERING_MODE_NATURAL    ) ||
-        !CUSTOM_RENDERING_PARAMS(Contrast02,   1.f, .2f, DWRITE_RENDERING_MODE_NATURAL    ) ||
-        !CUSTOM_RENDERING_PARAMS(GammaHalf,    .5f, 1.f, DWRITE_RENDERING_MODE_NATURAL    ) ||
-        !CUSTOM_RENDERING_PARAMS(Gamma2x,      2.f, 1.f, DWRITE_RENDERING_MODE_NATURAL    ) ||
-        !CUSTOM_RENDERING_PARAMS(GDIClassicContrast2x, 1.f, 2.f, DWRITE_RENDERING_MODE_GDI_CLASSIC) ||
-        !CUSTOM_RENDERING_PARAMS(GDIClassicContrast02, 1.f, .5f, DWRITE_RENDERING_MODE_GDI_CLASSIC) ||
-        !SUCCEEDED(pRT->CreateSolidColorBrush(
-            D2D1::ColorF(D2D1::ColorF::Black),
-            &pBlackBrush))) {
+        CUSTOM_RENDERING_PARAMS(GDIClassic,   1.f, 1.f, DWRITE_RENDERING_MODE_GDI_CLASSIC) &&
+        CUSTOM_RENDERING_PARAMS(GDINatural,   1.f, 1.f, DWRITE_RENDERING_MODE_GDI_NATURAL) &&
+        CUSTOM_RENDERING_PARAMS(Natural,      1.f, 1.f, DWRITE_RENDERING_MODE_NATURAL    ) &&
+        CUSTOM_RENDERING_PARAMS(Contrast2x,   1.f, 2.f, DWRITE_RENDERING_MODE_NATURAL    ) &&
+        CUSTOM_RENDERING_PARAMS(Contrast02,   1.f, .2f, DWRITE_RENDERING_MODE_NATURAL    ) &&
+        CUSTOM_RENDERING_PARAMS(GammaHalf,    .5f, 1.f, DWRITE_RENDERING_MODE_NATURAL    ) &&
+        CUSTOM_RENDERING_PARAMS(Gamma2x,      2.f, 1.f, DWRITE_RENDERING_MODE_NATURAL    ) &&
+        CUSTOM_RENDERING_PARAMS(GDIClassicContrast2x, 1.f, 2.f, DWRITE_RENDERING_MODE_GDI_CLASSIC) &&
+        CUSTOM_RENDERING_PARAMS(GDIClassicContrast02, 1.f, .5f, DWRITE_RENDERING_MODE_GDI_CLASSIC) &&
+        SUCCEEDED(pDWriteFactory->CreateTextFormat(
+            L"Consolas", // Font family name.
+            NULL,        // Font collection (NULL sets it to use the system font collection).
+            DWRITE_FONT_WEIGHT_REGULAR,
+            DWRITE_FONT_STYLE_NORMAL,
+            DWRITE_FONT_STRETCH_NORMAL,
+            ceilf(ConvertPointSizeToDIP(8)),
+            L"en-us",
+            &pTextFormat));
+}
+
+BOOL GetDesktopDpi() {
+    FLOAT fDpiX, fDpiY;
+    pD2DFactory->GetDesktopDpi(&fDpiX, &fDpiY);
+    dpiX = fDpiX;
+    dpiY = fDpiY;
+    dpiScaleX = fDpiX / baseDpi;
+    dpiScaleY = fDpiY / baseDpi;
+    Debug("Win32DirectWrite: GetDesktopDpi fDpiX=%f fDpiY=%f dpiScaleX=%f dpiScaleY=%f", fDpiX, fDpiY, dpiScaleX, dpiScaleY);
+    return TRUE;
+}
+
+BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct) {
+    RECT rc;
+    GetClientRect(hwnd, &rc);
+    D2D1_SIZE_U size = D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top);
+
+    if (!RecreateRenderTarget(hwnd, size)) {
         MessageBox(NULL,
             "OnCreate failed.",
             "OnCreate Error", MB_ICONERROR);
         return FALSE;
     }
+
     return TRUE;
+}
+
+void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify) {
+    Debug("Win32DirectWrite: OnCommand hwnd=0x%X id=%d hwndCtl=0x%X codeNotify=%d", hwnd, id, hwndCtl, codeNotify);
+    switch (id) {}
+}
+
+void OnSize(HWND hwnd, UINT state, int cx, int cy) {
+    Debug("Win32DirectWrite: OnSize hwnd=0x%X cx=%d cy=%d", hwnd, cx, cy);
+    if (state == SIZE_RESTORED || state == SIZE_MAXIMIZED) {}
+    D2D1_SIZE_U size = D2D1::SizeU(cx, cy);
+    RecreateRenderTarget(hwnd, size);
+    RedrawWindow(hwnd, NULL, NULL, RDW_ERASE | RDW_INVALIDATE);
+}
+
+void OnDpiChanged(HWND hwnd, UINT newDpiX, UINT newDpiY, const RECT& newScaledWindowRect) {
+    Debug("Win32DirectWrite: OnDpiChanged hwnd=0x%X newDpiX=%d newDpiY=%d", hwnd, newDpiX, newDpiY);
+    dpiX = newDpiX;
+    dpiY = newDpiY;
+    dpiScaleX = (FLOAT)dpiX / baseDpi;
+    dpiScaleY = (FLOAT)dpiY / baseDpi;
+    Debug("Win32DirectWrite: dpiScaleX=%f dpiScaleY=%f", dpiScaleX, dpiScaleY);
+    SetWindowPos(hwnd, HWND_TOP,
+        newScaledWindowRect.left,
+        newScaledWindowRect.top,
+        newScaledWindowRect.right  - newScaledWindowRect.left,
+        newScaledWindowRect.bottom - newScaledWindowRect.top,
+        SWP_NOZORDER | SWP_NOACTIVATE); // NOTE: Will cause OnSize and RecreateRenderTarget
+    RedrawWindow(hwnd, NULL, NULL, RDW_ERASE | RDW_INVALIDATE);
 }
 
 void OnPaintHDC(HWND hwnd) {
@@ -204,10 +291,9 @@ void OnPaintHDC(HWND hwnd) {
 }
 
 void OnPaint(HWND hwnd) {
+    //Debug("Win32DirectWrite: OnPaint hwnd=0x%X", hwnd);
     RECT rc;
     GetClientRect(hwnd, &rc);
-    FLOAT dpiScaleX = 1.0f;
-    FLOAT dpiScaleY = 1.0f;
     D2D1_RECT_F layoutRect = D2D1::RectF(
         static_cast<FLOAT>(rc.left) / dpiScaleX,
         static_cast<FLOAT>(rc.top)  / dpiScaleY,
@@ -260,10 +346,29 @@ void OnDestroy(HWND hwnd) {
     SafeRelease(&pD2DFactory);
 }
 
-void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify) {
-    switch (id) {}
+BOOL RecreateRenderTarget(const HWND hwnd, const D2D1_SIZE_U& size) {
+    SafeRelease(&pBlackBrush);
+    SafeRelease(&pRT);
+    D2D1_RENDER_TARGET_PROPERTIES renderTargetProperties = D2D1::RenderTargetProperties();
+    renderTargetProperties.dpiX = dpiX;
+    renderTargetProperties.dpiY = dpiY;
+    return
+        SUCCEEDED(pD2DFactory->CreateHwndRenderTarget(
+            renderTargetProperties,
+            D2D1::HwndRenderTargetProperties(
+                hwnd,
+                size),
+            &pRT)) &&
+        SUCCEEDED(pRT->CreateSolidColorBrush(
+            D2D1::ColorF(D2D1::ColorF::Black),
+            &pBlackBrush));
 }
 
-void OnSize(HWND hwnd, UINT state, int cx, int cy) {
-    if (state == SIZE_RESTORED || state == SIZE_MAXIMIZED) {}
+void Debug(LPCTSTR lpszFormat, ...) {
+    TCHAR szBuffer[512] = "\0";
+    va_list args;
+    va_start(args, lpszFormat);
+    _vsnprintf(szBuffer, sizeof(szBuffer)/sizeof(*szBuffer), lpszFormat, args);
+    OutputDebugString(szBuffer);
+    va_end(args);
 }
